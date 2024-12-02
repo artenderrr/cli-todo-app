2789cc66aa0cb00c867f6278a1259cc8e91f1e56
artender <artem1347929@gmail.com>
2024-12-02 13:54:00 +0300

    Move task display logic to a separate function

--- app/main.py
+++ app/main.py
@@ -45,14 +45,18 @@ class Tasks:
             return False
         return True
 
+def show_tasks():
+    """ Shows a list of tasks sorted by completion status """
+    tasks = sorted(Tasks().items, key=lambda task: task["done"])
+    if tasks:
+        for task in tasks:
+            click.echo(f"[{"x" if task["done"] else " "}] {task["name"]}")
+    else:
+        click.echo("You have no todos yet.")
+
 @click.group(invoke_without_command=True)
 @click.pass_context
 def cli(ctx):
     """ Entry point of application """
     if not ctx.invoked_subcommand:
-        tasks = sorted(Tasks().items, key=lambda task: task["done"])
-        if tasks:
-            for task in tasks:
-                click.echo(f"[{"x" if task["done"] else " "}] {task["name"]}")
-        else:
-            click.echo("You have no todos yet.")
+        show_tasks()
